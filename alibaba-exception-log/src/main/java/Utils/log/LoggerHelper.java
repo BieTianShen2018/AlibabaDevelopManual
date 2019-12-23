5eb862430f46a2a13ee2c52a2a5343bb4e51d5a6
BieTianShen2018 <120640519@qq.com>
2019-12-23 23:56:27 +0800

    create Logger  slf4j and logback

---
+++ alibaba-exception-log/src/main/java/Utils/log/LoggerHelper.java
@@ -0,0 +1,21 @@
+package Utils.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author admin
+ */
+public class LoggerHelper {
+    private  static final Logger logger= LoggerFactory.getLogger(LoggerHelper.class);
+
+    private LoggerHelper(){
+
+    }
+    public static void logInfo(String info){
+        if(logger.isInfoEnabled())
+        {
+            logger.info(info);
+        }
+    }
+}
--- alibaba-exception-log/src/main/java/com/alibaba/log/alibabaexceptionlog/AlibabaExceptionLogApplication.java
+++ alibaba-exception-log/src/main/java/com/alibaba/log/alibabaexceptionlog/AlibabaExceptionLogApplication.java
@@ -1,12 +1,18 @@
 package com.alibaba.log.alibabaexceptionlog;
 
+import Utils.log.LoggerHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Date;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"Utils.log"})
 public class AlibabaExceptionLogApplication {
 
     public static void main(String[] args) {
+        LoggerHelper.logInfo(new Date().toString()+":test666");
         SpringApplication.run(AlibabaExceptionLogApplication.class, args);
         //test code
     }
